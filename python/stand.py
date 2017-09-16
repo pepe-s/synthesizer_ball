58b22230e61b8a9c41dcaeed7688b4982ac7b272
Teppei <ta.te.chj.hi.g@gmail.com>
2017-09-16 23:26:53 +0900

    add volume_size

--- python/stand.py
+++ python/stand.py
@@ -18,25 +18,32 @@ class BallStand():
         self.spi.open(0, 0)
         
         self.volume_mode = 0
-        self.volume_mixer = 0
+        self.volume_size = 0
         self.mode = 0
+        self.volume = 0.0
 
     def setMode(self):
         interval = 250
-        if self.volume_mode < (interval * 1):
-            self.mode = 0
-        elif self.volume_mode < (interval * 2):
-            self.mode = 1
-        elif self.volume_mode < (interval * 3):
-            self.mode = 2
-        else:
-            self.mode = 3
+        a = self.volume_mode / interval
+        if a >= 4:
+            a = 3
+        self.mode = a
+
+    def setSize(self):
+        interval = 100
+        self.volume = (self.volume_size / interval) / 10.0 + 0.1
+        if self.volume > 1.0:
+            self.volume = 1.0
 
     def spin(self):
         while not self.stop_event.is_set():
-            resp = self.spi.xfer2([0x68, 0x00])
+            resp = self.spi.xfer2([0x78, 0x00])
             self.volume_mode = (resp[0]*256+resp[1]) & 0x3ff
             self.setMode()
+
+            resp = self.spi.xfer2([0x68, 0x00])
+            self.volume_size = (resp[0]*256+resp[1]) & 0x3ff
+            self.setSize()
             sleep(0.2)
 
     def run(self):

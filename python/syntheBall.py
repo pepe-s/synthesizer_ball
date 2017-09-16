b1da9dba436f70b6bcf6091efd869298e25297e0
Teppei <ta.te.chj.hi.g@gmail.com>
2017-09-16 23:25:56 +0900

    new code

--- python/syntheBall.py
+++ python/syntheBall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 
-2017.09.12 teppei
+2017.09.16 teppei
 
 """
 
@@ -12,7 +12,7 @@ from pygame import mixer
 from stand import BallStand
 from LEDctl.LED import FullColorLED
 import medal
-from sound import Game, SoundSE
+from sound import Game, SoundPlayer
 
 ROHM_RAW = "D1:D7:7E:49:81:20"
 SLEEP_SEC = 0.1
@@ -31,57 +31,82 @@ def main():
         print "led run"
 
         # soud settings
-        mixer.init(frequency = 48000, size = -16, channels = 2, buffer = 512)
-        se = SoundSE()
+        mixer.init(frequency = 22050, size = -16, channels = 2, buffer = 512)
+        se = SoundPlayer()
         game = Game()
         print "soud init ok"
         
         # ble connect
         sm = medal.SensorMedal(ROHM_RAW)
-        print "ble connected !!"
-
-        # notify subscribe
-        sm.writeCharacteristic(0x0b07, "\x01\x00", True)
-        print "write char OK"
         sm.getData()
+        print "sensor medal run"
 
         # start LED color change 
         led.standby = False
+        
+        # start OK voice
+        se.playMotion("kidou")
 
         cnt = 0
         while True:
+            # volume setting
+            if se.volume != stand.volume:
+                se.volume = stand.volume
+            if game.volume != stand.volume:
+                game.volume = stand.volume
+            
+            if stand.mode == 0:
+                # ? mode
+                if sm.ang_x > 100 or sm.ang_y > 100 or sm.ang_z > 100:
+                    se.playSE("byun_w")
+                    sleep(1)
+                if sm.ang_x < -100 or sm.ang_y < -100 or sm.ang_z < -100:
+                    se.playSE("byun_s")
+                    sleep(1)
+                sleep(SLEEP_SEC)
+
             if stand.mode == 1:
                 # music mode
-                if sm.ang_x > 100:
-                    se.play(0)
-                if sm.ang_y > 100:
-                    se.play(1)
-                if sm.ang_z > 100:
-                    se.play(2)
-                if sm.ang_x < -100:
-                    se.play(3)
-                if sm.ang_y < -100:
-                    se.play(4)
-                if sm.ang_z < -100:
-                    se.play(5)
+                if medal.ang_x > 100:
+                    se.playSE("do")
+                if medal.ang_y > 100:
+                    se.playSE("re")
+                if medal.ang_z > 100:
+                    se.playSE("mi")
+                if medal.ang_x < -100:
+                    se.playSE("fa")
+                if medal.ang_y < -100:
+                    se.playSE("so")
+                if medal.ang_z < -100:
+                    se.playSE("ra")
                 sleep(SLEEP_SEC)
-                    
+
             elif stand.mode == 2:
+                # motion mode
+                # if m.motion == 1:
+                #     se.playSE("roll")
+                # elif m.motion == 2:
+                #     se.playMotion("hyaa", 1.2)
+                # elif m.motion == 3:
+                #     se.playMotion("kyaa", 1.5)
+                # elif m.motion == 4:
+                #     se.playMotion("gyaa", 2.0)
+                sleep(0.15)
+
+            elif stand.mode == 3:
                 # game mode
-                while stand.mode == 2:
+                while stand.mode == 3:
                     if not game.gaming:
-                        if sm.accel_z < -3.0:
+                        if sm.ang_x < 100.0:
                             game.gameStart()
                     else:
                         if game.isLimit():
                             break
-                
+                    if game.volume != stand.volume:
+                        game.changeVolume(stand.volume)
+                    sleep(0.2)
                 if game.gaming:
                     game.gameStop()
-            else:
-                print "none mode %d" % cnt
-                cnt += 1
-                sleep(0.5)
     
     except KeyboardInterrupt:
         print "Ctl - c"
@@ -90,6 +115,8 @@ def main():
         traceback.print_exc()
 
     finally:
+        if game.gaming:
+            game.gameStop()
         mixer.quit()
         
         if not led is None:
